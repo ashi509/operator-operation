be82cc422843311f7db6ed0b0e9618a90dc9ae82
Ashish Kumar <ashishkumar31101999@gmail.com>
2022-11-26 14:03:38 +0530

    added new feature

---
+++ src/operation/Class1.java
@@ -0,0 +1,11 @@
+package operation;
+
+public class Class1 {
+    public static void main(String args[]){
+        int num1 =4;
+
+        int num3=9;
+        int sum=num1+num3;
+        System.out.println("sum-"+sum);
+    }
+}
